110a548172d07d6f0d7858c0467afbb1ed864691
mdig0003 <96048719+mdig0003@users.noreply.github.com>
2023-10-03 20:53:37 +1100

    files

---
+++ js/histogram.js
@@ -0,0 +1,6 @@
+var histogram = "json/histogram.json";
+vegaEmbed('#histogram', histogram).then(function(result) {
+
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+
+}).catch(console.error);
---
+++ js/treemap_AQI.js
@@ -0,0 +1,6 @@
+var vis = "json/treemap.json";
+vegaEmbed('#vis', vis).then(function(result) {
+
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+
+}).catch(console.error);
